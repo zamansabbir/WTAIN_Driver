647a527b0d16d858db6812ccd05782a36aa3de01
zamansabbir <sabbir.zaman@hotmail.com>
2025-04-02 15:23:22 -0700

    Adding doxygen documentation

---
+++ docs/html/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sendcommand_5',['SendCommand',['../analog__input_8c.html#aa43950b02cb881b2ac3ff29c9ecc979e',1,'analog_input.c']]],
+  ['sendcommandreadresponse_6',['SendCommandReadResponse',['../analog__input_8c.html#aedc5a955554ab61ef899c3c32bed253a',1,'analog_input.c']]]
+];
---
+++ docs/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['analog_5finput_2ec_7',['analog_input.c',['../analog__input_8c.html',1,'']]]
+];
