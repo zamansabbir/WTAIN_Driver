c74adb0e6facfe0e690659d94f322c6bd8e31897
zamansabbir <sabbir.zaman@hotmail.com>
2025-04-02 15:37:51 -0700

    Removed latex and html files, included accidentally

--- docs/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['readchannel_3',['ReadChannel',['../analog__input_8c.html#a415d66ad892f01839c205f3be19f4d8c',1,'analog_input.c']]],
-  ['readresponse_4',['ReadResponse',['../analog__input_8c.html#aaee16e5a2c0757d384276a941b6a253f',1,'analog_input.c']]]
-];
--- docs/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['configuredefaults_8',['ConfigureDefaults',['../analog__input_8c.html#ada0fbeb0c95d2098226d5c67eca659df',1,'analog_input.c']]],
-  ['configureserial_9',['ConfigureSerial',['../analog__input_8c.html#acce1ef27635fc6758bf925e0f6460f2b',1,'analog_input.c']]]
-];
